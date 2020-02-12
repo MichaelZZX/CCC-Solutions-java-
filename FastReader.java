9610ed8d300f4a2bc004b5c28fc28ee8fe8097e8
and-chen <54198695+and-chen@users.noreply.github.com>
2020-02-12 04:20:36 -0800

    Create FastReader.java

---
+++ FastReader.java
@@ -0,0 +1,43 @@
+class FastReader { 
+	BufferedReader br; 
+	StringTokenizer st; 
+
+	public FastReader() { 
+		br = new BufferedReader(new InputStreamReader(System.in)); 
+	} 
+
+	public String next() { 
+		while (st == null || !st.hasMoreElements()) { 
+			try { 
+				st = new StringTokenizer(br.readLine()); 
+			} 
+			catch (IOException  e) { 
+				e.printStackTrace(); 
+			} 
+		}
+		return st.nextToken(); 
+	} 
+
+	public int nextInt() { 
+		return Integer.parseInt(next()); 
+	} 
+
+	long nextLong() { 
+		return Long.parseLong(next()); 
+	} 
+
+	double nextDouble() { 
+		return Double.parseDouble(next()); 
+	} 
+
+	String nextLine() { 
+		String str = ""; 
+		try	{ 
+			str = br.readLine(); 
+		} 
+		catch (IOException e) { 
+			e.printStackTrace(); 
+		} 
+		return str; 
+	} 
+} 
